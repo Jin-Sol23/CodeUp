80923ff107c47af4a85677a68a5ee0354743f0df
Jin-Sol23 <ljs12132@naver.com>
2023-11-19 19:14:16 +0900

    CodeUp

--- 88.py
+++ 88.py
@@ -1,11 +1,11 @@
+# 종합 - 수 나열하기1
+# 입력 : 시작값(a), 등차값(d), 몇 번째 수인지 의미하는 정수(n)
+# a, d, n이 공백을 두고 입력됨
+# 출력 : n번째 수를 출력함
+
 a, d, n = input().split()
 a = int(a)
 d = int(d)
 n = int(n)
-s = 0
 
-for i in range(a):
-    i = d + a
-    a += 1
-    s = s + i
-    print(s)
+print(a+d*(n-1)) # 검색
