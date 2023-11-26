365378fcf53c6cab747d01ec1b9e9e439b676cb4
Jin-Sol23 <ljs121345@gmail.com>
2023-11-26 22:22:48 +0900

    CodeUp

--- 96.py
+++ 96.py
@@ -16,5 +16,17 @@ for i in range(n):
         else:
             d[int(x)][j]=0
         print(d[i][j], end=' ')
-    print()
+    print(i)
     
+for i in range(n) :
+  x,y=input().split()
+  for j in range(1, 20) :
+    if d[j][int(y)]==0 :
+      d[j][int(y)]=1
+    else :
+      d[j][int(y)]=0
+
+    if d[int(x)][j]==0 :
+      d[int(x)][j]=1
+    else :
+      d[int(x)][j]=0
